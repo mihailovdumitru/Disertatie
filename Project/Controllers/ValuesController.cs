6c3f7a336dea69799760f411a282b2782c93db10
root <mihailovdumitru@gmail.com>
2018-05-01 22:10:01 +0300

    Working project

--- Project/Controllers/ValuesController.cs
+++ Project/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Prj.Controllers
+namespace Project.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
@@ -13,14 +13,14 @@ namespace Prj.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value3" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Dima";
+            return "value";
         }
 
         // POST api/values
--- Project/Startup.cs
+++ Project/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Prj
+namespace Project
 {
     public class Startup
     {
