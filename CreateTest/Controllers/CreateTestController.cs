e5fce49e871b4e7222bd514aed0b45dfed3f31f1
mihailovdumitru <mihailovdumitru@gmail.com>
2018-05-26 13:24:16 +0300

    Add model for CreateTest

---
+++ CreateTest/Controllers/CreateTestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CreateTest.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreateTest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CreateTest")]
+    public class CreateTestController : Controller
+    {
+        // GET: api/CreateTest
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CreateTest/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(HttpRequestMessage request, int id)
+        {
+            return "";
+        }
+        
+        // POST: api/CreateTest
+        [HttpPost]
+        public void Post([FromBody]Test testObject)
+        {
+            Console.WriteLine(testObject.Lecture);
+        }
+        
+        // PUT: api/CreateTest/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CreateTest/Model/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateTest.Model
+{
+
+    public struct Answer
+    {
+        public string Content { get; set; }
+        public int Points { get; set; }
+    }
+
+    public struct Question
+    {
+        public string Content { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+    public class Test
+    {
+        public List<Question> Questions { get; set; }
+        public string Lecture { get; set; }
+    }
+}
